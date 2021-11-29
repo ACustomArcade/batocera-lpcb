b94635e0b27e540dd7f7a099ff5a108fb8d5ee9e
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-11-28 20:24:19 -0500

    Create lpcb-init.sh

---
+++ userdata/system/lpcb-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PID_FILE=/var/run/lpcb-volume.pid
+
+case "$1" in
+start)
+  /userdata/system/lpcb-volume.py &
+  echo $! > $PID_FILE
+;;
+stop)
+  kill -9 `cat ${PID_FILE}`
+;;
+esac
