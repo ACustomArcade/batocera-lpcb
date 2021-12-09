11886fbcf22cdb45519ae321d0a819f437e25153
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-08 22:15:07 -0500

    Migrate to GPIOnext

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget https://github.com/mholgatem/GPIOnext/archive/refs/heads/master.zip
+unzip master.zip
+mv GPIOnext-master/ /userdata/system/GPIOnext
+rm -f master.zip
+
+curl -kLo /userdata/system/gpionext-init.sh https://raw.githubusercontent.com/ACustomArcade/batocera-lpcb/main/userdata/system/gpionext-init.sh
+chmod +x /userdata/system/gpionext-init.sh
+grep -qxF '/userdata/system/gpionext-init.sh $1' /userdata/system/custom.sh 2> /dev/null || echo '/userdata/system/gpionext-init.sh $1' >> /userdata/system/custom.sh
+
+curl -kLo /userdata/system/GPIOnext/config/config.db https://raw.githubusercontent.com/ACustomArcade/batocera-lpcb/main/userdata/system/GPIOnext/config/config.db
+
+echo "GPIOnext installed. Please reboot your system!"
