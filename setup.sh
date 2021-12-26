f8631a4611dc1c2fc10d14fc067ad7059ef54f67
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-26 11:18:19 -0500

    Detect TH boards

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+version=1  #increment this as the script is updated
+
+if [[ -f "/userdata/system/lpcb/lpcb-version" ]]; then
+  echo "Existing Pixelcade installation detected, checking version..."
+    read -r currentVersion</userdata/system/lpcb/lpcb-version
+    if [[ $currentVersion -lt $version ]]; then
+      echo "Older lpcb version detected, now upgrading..."
+      /userdata/system/lpcb/init/gpionext-init.sh stop
+    fi
+  else
+    while true; do
+      printf "${magenta}Your existing Pixelcade installation will be deleted including any custom artwork you've added, do you want to re-install? (y/n): ${white}"
+      read yn
+      case $yn in
+        [Yy]* ) /userdata/system/lpcb/init/gpionext-init.sh stop; rm -rf /userdata/system/lpcb; rm -rf /userdata/system/GPIOnext; break;;
+        [Nn]* ) exit; break;;
+        * ) echo "Please answer y or n";;
+      esac
+    done
+  fi
+fi
+
 cd /tmp
 curl -kLO /tmp/master.zip https://github.com/mholgatem/GPIOnext/archive/refs/heads/master.zip
 unzip -q master.zip
@@ -17,6 +39,24 @@ grep -qxF '/userdata/system/lpcb/init/gpionext-init.sh $1' /userdata/system/cust
 
 curl -kLo /userdata/system/GPIOnext/config/config.db https://github.com/ACustomArcade/batocera-lpcb/raw/main/userdata/system/GPIONext/config/config.db
 
+echo 17 2>/dev/null > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio17/direction
+echo "Detecting LPCB revision..."
+echo "You will need to be in front of the ALU to continue."
+echo
+echo "Please hold down the Volume Down button on the ALU."
+echo "DO NOT release the button until you are told!"
+read -p "Press enter to read the buttons"
+sleep 2
+vol_down = $(cat /sys/class/gpio/gpio17/value)
+echo "You may now release the Volume Down button."
+read -p "Press enter to continue..."
+
+if [[ $vol_down == 1 ]]; then
+  sqlite3 /userdata/system/GPIOnext/config/config.db 'UPDATE \'GPIOnext\' SET pins = \'11\' WHERE name = \'Volume Up\';'
+  sqlite3 /userdata/system/GPIOnext/config/config.db 'UPDATE \'GPIOnext\' SET pins = \'13\' WHERE name = \'Volume Down\';'
+fi
+
 /userdata/system/lpcb/init/gpionext-init.sh start
 
 echo "GPIOnext installed!"
@@ -30,3 +70,6 @@ if ls /dev/ttyACM0 | grep -q '/dev/ttyACM0'; then
 else
    echo "Sorry, Pixelcade LED Marquee was not detected. If you have one, please ensure Pixelcade is USB connected to your Pi and the toggle switch on the Pixelcade board is pointing towards USB, exiting..."
 fi
+
+#let's write the version so the next time the user can try and know if they need to upgrade
+echo $version > /userdata/system/lpcb/lpcb-version
