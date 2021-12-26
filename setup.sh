039ca62c03f8bdc31d387377e92e689f92dafd62
n-i-x <440158+n-i-x@users.noreply.github.com>
2021-12-26 10:49:34 -0500

    Update paths and run pixelcade installer if detected

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-wget https://github.com/mholgatem/GPIOnext/archive/refs/heads/master.zip
-unzip master.zip
+cd /tmp
+curl -kLO /tmp/master.zip https://github.com/mholgatem/GPIOnext/archive/refs/heads/master.zip
+unzip -q master.zip
 mv GPIOnext-master/ /userdata/system/GPIOnext
-rm -f master.zip
 
-curl -kLo /userdata/system/gpionext-init.sh https://raw.githubusercontent.com/ACustomArcade/batocera-lpcb/main/userdata/system/gpionext-init.sh
-chmod +x /userdata/system/gpionext-init.sh
-grep -qxF '/userdata/system/gpionext-init.sh $1' /userdata/system/custom.sh 2> /dev/null || echo '/userdata/system/gpionext-init.sh $1' >> /userdata/system/custom.sh
+mkdir -p /userdata/system/lpcb/init
+curl -kLo /userdata/system/lpcb/init/gpionext-init.sh https://raw.githubusercontent.com/ACustomArcade/batocera-lpcb/main/userdata/system/gpionext-init.sh
+chmod +x /userdata/system/lpcb/init/gpionext-init.sh
+grep -qxF '/userdata/system/lpcb/init/gpionext-init.sh $1' /userdata/system/custom.sh 2> /dev/null || echo '/userdata/system/lpcb/init/gpionext-init.sh $1' >> /userdata/system/custom.sh
 
 curl -kLo /userdata/system/GPIOnext/config/config.db https://github.com/ACustomArcade/batocera-lpcb/raw/main/userdata/system/GPIONext/config/config.db
 
-/userdata/system/gpionext-init.sh start
+/userdata/system/lpcb/init/gpionext-init.sh start
 
 echo "GPIOnext installed!"
+
+echo "Detecting Pixelcade..."
+# let's detect if Pixelcade is connected
+if ls /dev/ttyACM0 | grep -q '/dev/ttyACM0'; then
+   echo "Pixelcade LED Marquee Detected!"
+   echo "Running the Pixelcade installer..."
+   bash <(curl -s https://raw.githubusercontent.com/ACustomArcade/batocera-pixelcade/main/batocera_setup.sh)
+else
+   echo "Sorry, Pixelcade LED Marquee was not detected. If you have one, please ensure Pixelcade is USB connected to your Pi and the toggle switch on the Pixelcade board is pointing towards USB, exiting..."
+fi
